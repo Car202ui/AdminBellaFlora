0f40d6abf72916b6de871e22f84bbbef2d0b49f5
Car202ui <qute2929@gmail.com>
2022-05-29 13:12:37 -0500

    proyecto backend bella flora

---
+++ AdminBellaFlora/src/main/java/com/company/adminBellaFlora/AdminBellaFloraApplication.java
@@ -0,0 +1,13 @@
+package com.company.adminBellaFlora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminBellaFloraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminBellaFloraApplication.class, args);
+	}
+
+}
---
+++ AdminBellaFlora/src/test/java/com/company/adminBellaFlora/AdminBellaFloraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.adminBellaFlora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminBellaFloraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
